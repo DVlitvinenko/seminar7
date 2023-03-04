9675f6a09e96beaf16164c65f0626b6217612929
Sergei Kamianetskii <i@ksergey.ru>
2023-03-04 15:50:15 +0300

    Обновлёны всё

--- Program.cs
+++ Program.cs
@@ -1,52 +1,51 @@
 ﻿int Input(string msg)
 {
-    Console.Write(msg + " ");
-    return Convert.ToInt32(Console.ReadLine());
+  Console.Write(msg + " ");
+  return Convert.ToInt32(Console.ReadLine());
 }
 
 int[,] CreateArray(int a, int b)
 {
-    int[,] matrix = new int[a, b];
-    return matrix;
+  int[,] matrix = new int[a, b];
+  return matrix;
 }
 
 void RandomArray(int[,] NameArray)
 {
-    for (int i = 0; i < NameArray.GetLength(0); i++)
+  for (int i = 0; i < NameArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < NameArray.GetLength(1); j++)
     {
-        for (int j = 0; j < NameArray.GetLength(1); j++)
-        {
-            NameArray[i, j] = new Random().Next(1, 1000);
-        }
+      NameArray[i, j] = new Random().Next(1, 1000);
     }
+  }
 }
 
 void PrintArray(int[,] NameArray)
 {
-    for (int i = 0; i < NameArray.GetLength(0); i++)
+  for (int i = 0; i < NameArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < NameArray.GetLength(1); j++)
     {
-        for (int j = 0; j < NameArray.GetLength(1); j++)
-        {
-            Console.Write(NameArray[i, j] + " ");
-        }
-        Console.WriteLine();
+      Console.Write(NameArray[i, j] + " ");
     }
+    Console.WriteLine();
+  }
 }
 
-void NewArray (int [,] NameArray)
-        {
-            int i, j, temp;
-            int n = NameArray.GetLength(0); // кол-во строк
-            int m = NameArray.GetLength(1); // кол-во столбцов
-            
-            for (j = 0; j < m; j++) // перебираем столбцы
-            {
-                temp=NameArray[0, j];
-                NameArray[0, j]=NameArray[n - 1, j];
-                NameArray[n - 1, j] = temp;
-            }
-            
-        }
+void NewArray(int[,] NameArray)
+{
+  int i, j, temp;
+  int n = NameArray.GetLength(0); // кол-во строк
+  int m = NameArray.GetLength(1); // кол-во столбцов
+
+  for (j = 0; j < m; j++) // перебираем столбцы
+  {
+    temp = NameArray[0, j];
+    NameArray[0, j] = NameArray[n - 1, j];
+    NameArray[n - 1, j] = temp;
+  }
+}
 
 int A = Input("Введите количество строк массива");
 int B = Input("Введите количество столбцов массива");
@@ -54,7 +53,7 @@ int B = Input("Введите количество столбцов массив
 int[,] matr = CreateArray(A, B);
 RandomArray(matr);
 PrintArray(matr);
-NewArray (matr);
+NewArray(matr);
 Console.WriteLine();
 PrintArray(matr);
 
